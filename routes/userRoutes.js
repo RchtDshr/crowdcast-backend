9729c3c080b56ea840bd6de18c9b1c46d10d5909
Arindam <17arindambera@gmail.com>
2024-09-25 02:04:13 +0530

    signin and signout routes added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const {
     signup,
-    verifyOtp
+    verifyOtp,
+    signin,
+    signout
 } = require('../controller/UserController');
+
 
 const router = express.Router();
 
 router.post("/verify-otp", verifyOtp);
 router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/signout", signout);
+
+
 
 module.exports = router;
