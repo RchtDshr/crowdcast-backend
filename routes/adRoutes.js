450199711cd33bb028d3bce58d6fbff0cf7e43c0
Arindam <17arindambera@gmail.com>
2024-11-20 05:23:24 +0530

    fix:video issue

--- routes/adRoutes.js
+++ routes/adRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { createAd, getUserAds, uploadFiletoCloudinary, removeFilefromCloudinary, getAdById } = require("../controller/AdController");
 const { getUserData } = require("../controller/UserController");
+// const multer = require('multer');
+// const upload = multer({ dest: 'ads/' });
+
 const createUploadMiddleware = require('../middleware/cloudinary');
+const { getAdIdsByGrouping } = require("../controller/LocationController");
 const upload = createUploadMiddleware('ads');
 
 const router = express.Router();
@@ -10,7 +14,9 @@ router.post("/create-ad", createAd);
 router.post("/user", getUserData);
 router.post('/upload', upload.single('file'), uploadFiletoCloudinary);
 router.post('/remove',upload.single('file'), removeFilefromCloudinary);
-
+router.post('/fetchAdIds',getAdIdsByGrouping);
 router.get("/getdetails", getUserAds);
+router.get('/:adId', getAdById);
+
 
 module.exports = router;
